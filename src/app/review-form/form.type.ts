ff5738fa065d456992c7b63f6b32be5b7879e211
Tsotne <tsotnebasilidze@gmail.com>
2025-04-11 18:00:20 +0400

    styles

--- src/app/review-form/form.type.ts
+++ src/app/review-form/form.type.ts
@@ -3,7 +3,7 @@ import { FormArray, FormControl, FormGroup } from "@angular/forms";
 export interface Registerform {
     fullName: FormControl<string | null>,
     Email: FormControl<string | null>,
-    Number: FormControl<number | null>,
+    Number: FormControl<string | null>,
     Product: FormControl<string | null>,
     Review: FormGroup<ReviewGroup>,
     termsAndConds: FormControl<boolean | null>
